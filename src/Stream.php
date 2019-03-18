85dce5f3d6449e239fda6350b6512c01c643c686
Alexandre <gealex55@gmail.com>
2019-03-18 12:17:02 +0100

    v1.0.0

---
+++ src/Stream.php
@@ -0,0 +1,364 @@
+<?php
+
+namespace SitPHP\Resources;
+
+
+class Stream extends Resource
+{
+
+    // Internal properties
+    protected $handle;
+
+    // User properties
+    protected $path;
+    protected $mode;
+    protected $context;
+
+    /**
+     * Resource constructor.
+     *
+     * @param string $path
+     * @param string|null $open_mode
+     * @param null $context
+     */
+    function __construct(string $path, string $open_mode = null, $context = null)
+    {
+        $this->path = $path;
+        $this->mode = $open_mode;
+        $this->context = $context;
+    }
+
+    /**
+     * Return handle
+     *
+     * @return resource
+     * @throws \Exception
+     */
+    function getHandle(){
+        if(!isset($this->handle)){
+            if(!$handle = fopen($this->path, $this->mode, $this->context)){
+                throw new \Exception('Could not open resource "'.$this->path.'"');
+            }
+            $this->handle = $handle;
+        }
+        return $this->handle;
+    }
+
+    /**
+     * Return file name
+     *
+     * @return string
+     */
+    function getPath(){
+        return $this->path;
+    }
+
+    function getMode(){
+        return $this->mode;
+    }
+
+    function getContext(){
+        return $this->context;
+    }
+
+    /*
+     * Type methods
+     */
+    /**
+     * Return file type
+     *
+     * @return string
+     * @see http://php.net/manual/function.filetype.php
+     */
+    function getType()
+    {
+        if (preg_match('#^#', $this->path)) {
+            $type = $this->resolveStatType();
+        } else {
+            $type = filetype($this->path);
+        }
+        return $type;
+    }
+    function resolveStatType(){
+        $mode = $this->getStat()['mode'] & 0170000;
+        if ($mode == 0010000) {
+            $type = 'fifo';
+        } else if ($mode == 0020000) {
+            $type = 'char';
+        } else if ($mode == 0040000) {
+            $type = 'dir';
+        } else if ($mode == 0060000) {
+            $type = 'block';
+        } else if ($mode == 0100000) {
+            $type = 'file';
+        } else if ($mode == 0120000) {
+            $type = 'link';
+        } else if ($mode == 0140000) {
+            $type = 'socket';
+        } else {
+            $type = 'unknown';
+        }
+        return $type;
+    }
+
+    /**
+     * Check if is named pipe
+     *
+     * @return bool
+     */
+    function isPipe()
+    {
+        return $this->getType() == 'fifo';
+    }
+
+    /**
+     * Check if is character device
+     * @return bool
+     */
+    function isChar()
+    {
+        return $this->getType() == 'char';
+    }
+
+    /**
+     * Check if is dir
+     *
+     * @return bool
+     * @see http://php.net/manual/function.is-dir.php
+     */
+    function isDir()
+    {
+        return $this->getType() == 'dir';
+    }
+
+    /**
+     * CHeck if is standard file
+     *
+     * @return bool
+     * @see http://php.net/manual/function.is-file.php
+     */
+    function isFile()
+    {
+        return $this->getType() == 'file';
+    }
+
+    /**
+     * Check if is link
+     *
+     * @return bool
+     * @see http://php.net/manual/function.is-link.php
+     */
+    function isLink()
+    {
+        return $this->getType() == 'link';
+    }
+
+    function isBlock(){
+        return $this->getType() == 'block';
+    }
+
+    /**
+     * Check if stream or url is a tty
+     *
+     * @return bool
+     * @see http://php.net/manual/function.stream-isatty.php
+     * @throws \Exception
+     */
+    function isAtty()
+    {
+        if(version_compare(phpversion(), '7.2', '<')){
+            return @posix_isatty($this->getHandle());
+        }
+        return stream_isatty($this->getHandle());
+    }
+
+
+    /**
+     * Check if stream is local
+     *
+     * @return bool
+     */
+    function isLocal()
+    {
+        return stream_is_local($this->path);
+    }
+
+    function getResourceType()
+    {
+        return get_resource_type($this->getHandle());
+    }
+
+    /**
+     * Return file stats
+     *
+     * @return array
+     * @see http://php.net/manual/function.fstat.php
+     * @throws \Exception
+     */
+    function getStat()
+    {
+        return fstat($this->getHandle());
+    }
+
+    /**
+     * Clear file stat cache
+     *
+     * @param bool $clear_realpath_cache
+     * @see http://php.net/manual/function.clearstatcache.php
+     */
+    function clearStatCache($clear_realpath_cache = false)
+    {
+        clearstatcache($clear_realpath_cache, $this->path);
+    }
+
+    /*
+    * Read methods
+    */
+    /**
+     * Return one byte
+     *
+     * @return bool|string
+     * @see http://php.net/manual/function.fgetc.php
+     * @throws \Exception
+     */
+    function readByte()
+    {
+        return fgetc($this->getHandle());
+    }
+
+    /**
+     * Return bytes of given length
+     *
+     * @param int $length
+     * @return bool|string
+     * @see http://php.net/manual/function.fread.php
+     * @throws \Exception
+     */
+    function readBytes(int $length)
+    {
+        return fread($this->getHandle(), $length);
+    }
+
+    /**
+     * Return line
+     *
+     * @param int|null $length
+     * @return bool|string
+     * @see http://php.net/manual/function.fgets.php
+     * @throws \Exception
+     */
+    function readLine(int $length = null)
+    {
+        return isset($length) ? fgets($this->getHandle(), $length) : fgets($this->getHandle());
+    }
+
+
+
+    /*
+     * Write methods
+     */
+    /**
+     * Write message to file
+     *
+     * @param string $message
+     * @return bool|int
+     * @see http://php.net/manual/function.fwrite.php
+     * @throws \Exception
+     */
+    function put(string $message)
+    {
+        return fwrite($this->getHandle(), $message);
+    }
+
+    function getContents(int $maxlength = -1, int $offset = -1)
+    {
+        return stream_get_contents($this->getHandle(), $maxlength, $offset);
+    }
+
+    /**
+     * Check if pointer is at end of file
+     *
+     * @return bool
+     * @see http://php.net/manual/function.feof.php
+     * @throws \Exception
+     */
+    function isEndOfFile()
+    {
+        return feof($this->getHandle());
+    }
+
+    /**
+     * Flush the output to file
+     *
+     * @return bool
+     * @see http://php.net/manual/function.fflush.php
+     * @throws \Exception
+     */
+    function flush()
+    {
+        return fflush($this->getHandle());
+    }
+
+    /**
+     * Write remaining data to output buffer
+     *
+     * @return bool|int
+     * @see http://php.net/manual/function.fpassthru.php
+     * @throws \Exception
+     */
+    function passThru()
+    {
+        return fpassthru($this->getHandle());
+    }
+
+    /**
+     * Move pointer to offset position
+     *
+     * @param int $offset
+     * @param int $whence
+     * @return int
+     * @see http://php.net/manual/function.fseek.php
+     * @throws \Exception
+     */
+    function setPointerPosition(int $offset, int $whence = SEEK_SET)
+    {
+        return fseek($this->getHandle(), $offset, $whence);
+    }
+
+    /**
+     * Return file pointer position
+     *
+     * @return bool|int
+     * @see http://php.net/manual/function.ftell.php
+     * @throws \Exception
+     */
+    function getPointerPosition()
+    {
+        return ftell($this->getHandle());
+    }
+
+    /**
+     * Rewind the position of file pointer
+     *
+     * @return bool
+     * @see http://php.net/manual/function.rewind.php
+     * @throws \Exception
+     */
+    function rewind()
+    {
+        return rewind($this->getHandle());
+    }
+
+    /**
+     * Close file pointer
+     *
+     * @return bool
+     * @see http://php.net/manual/function.fclose.php
+     * @throws \Exception
+     */
+    function close()
+    {
+        return fclose($this->getHandle());
+    }
+
+}
---
+++ tests/StreamTest.php
@@ -0,0 +1,166 @@
+<?php
+
+use Doublit\TestCase;
+use SitPHP\Resources\Stream;
+
+class StreamTest extends TestCase
+{
+    function testGetHandleShouldReturnAResource(){
+        $stream = new Stream('php://memory');
+        $this->assertIsResource($stream->getHandle());
+    }
+    function testGetPath(){
+        $stream = new Stream('php://memory');
+        $this->assertEquals('php://memory', $stream->getPath());
+    }
+    function testGetMode(){
+        $stream = new Stream('php://memory', 'r+');
+        $this->assertEquals('r+', $stream->getMode());
+    }
+    function testGetContext(){
+        $context = stream_context_create();
+        $stream = new Stream('php://memory', 'r+', $context);
+        $this->assertEquals($context, $stream->getContext());
+    }
+
+    /*
+     * Test is local
+     */
+    function testIsLocal(){
+        $stream_1 = new Stream('php://stdin');
+        $stream_2 = new Stream('http://www.example.com');
+        $this->assertTrue($stream_1->isLocal());
+        $this->assertFalse($stream_2->isLocal());
+    }
+
+    /*
+     * Test types
+     */
+    function testIsPipe(){
+        $stream = new Stream('php://stdin');
+        $this->assertTrue($stream->isPipe());
+    }
+    function testIsFile(){
+        $stream = new Stream(__FILE__, 'r');
+        $this->assertTrue($stream->isFile());
+    }
+    function testIsDir(){
+        $stream = new Stream(__DIR__, 'r');
+        $this->assertTrue($stream->isDir());
+    }
+    function testIsBlock(){
+        $stream = new Stream(__FILE__, 'r');
+        $this->assertFalse($stream->isBlock());
+    }
+    function testIsChar(){
+        $stream = new Stream(__FILE__, 'r');
+        $this->assertFalse($stream->isChar());
+    }
+    function testIsLink(){
+        $stream = new Stream(__FILE__, 'r');
+        $this->assertFalse($stream->isLink());
+    }
+    function testIsAtty(){
+        $stream = new Stream(__FILE__, 'r');
+        $this->assertFalse($stream->isAtty());
+    }
+    function testGetResourceTypeShouldReturnType(){
+        $stream = new Stream('php://memory');
+        $this->assertEquals('stream', $stream->getResourceType());
+    }
+
+
+    /*
+   * Test is end of file
+   */
+    function testIsEndOfFile(){
+        $stream = new Stream('php://memory', 'w+');
+        $stream->put('write');
+        $this->assertFalse($stream->isEndOfFile());
+        $stream->readLine();
+        $this->assertTrue($stream->isEndOfFile());
+    }
+
+
+    /*
+     * Test write
+     */
+    function testWrite(){
+        $stream = new Stream('php://memory', 'w+r+');
+        $stream->put('write');
+        $stream->rewind();
+        $this->assertEquals('write', $stream->getContents());
+    }
+
+
+    /*
+     * Test read
+     */
+    function testReadByte(){
+        $stream = new Stream('php://memory', 'w+r+');
+        $stream->put('byte');
+        $stream->rewind();
+        $this->assertEquals('b',$stream->readByte());
+        $this->assertEquals('y',$stream->readByte());
+        $this->assertEquals('t',$stream->readByte());
+        $this->assertEquals('e',$stream->readByte());
+        $this->assertEquals('',$stream->readByte());
+    }
+
+    function testReadBytes(){
+        $stream = new Stream('php://memory', 'w+r+');
+        $stream->put('bytes');
+        $stream->rewind();
+        $this->assertEquals('byte',$stream->readBytes(4));
+    }
+
+    function testReadLine(){
+        $stream = new Stream('php://memory', 'w+r+');
+        $stream->put('bytes'."\n".'on'."\n".'lines');
+        $stream->rewind();
+        $stream->readBytes(2);
+        $this->assertEquals('tes'."\n",$stream->readLine());
+    }
+
+    /*
+     * Test pass thru
+     */
+    function testPassThru(){
+        $stream = new Stream('php://memory', 'w+r+');
+        $stream->put('bytes'."\n".'on'."\n".'lines');
+        $stream->rewind();
+        $stream->readBytes(2);
+        $this->assertEquals(12,$stream->passThru('tes'."\n".'on'."\n".'lines'));
+    }
+
+    /*
+     * Test pointer
+     */
+
+    function testPointer(){
+        $stream = new Stream('php://memory', 'r+w+');
+        $stream->put('write');
+        $stream->setPointerPosition(1);
+        $this->assertEquals('rite', $stream->getContents());
+        $this->assertEquals(5, $stream->getPointerPosition());
+    }
+
+    /*
+     * Test tell
+     */
+    function testTell(){
+        $stream = new Stream('php://memory', 'w+');
+        fwrite($stream->getHandle(), 'write');
+        $stream->setPointerPosition(1);
+        $this->assertEquals(1, $stream->getPointerPosition());
+    }
+
+    /*
+     * Test close
+     */
+    function testClose(){
+        $stream = new Stream('php://memory');
+        $stream->close();
+        $this->assertFalse(is_resource($stream->getHandle()));
+    }
+}
